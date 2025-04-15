bb1a9ff1ae9c7f7b5b486d4fa5055ce83257e601
Jacob Brumfield <brumfijb@mail.uc.edu>
2025-04-15 13:13:12 -0400

    Add project files.

---
+++ Persian_Assignment11/mainPackage/main.py
@@ -0,0 +1,13 @@
+# File Name : main.py
+# Student Name: Jacob Brumfield, Justin Ganduri
+# email:  brumfijb@mail.uc.edu, gandurpn@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:   4/17/2025
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Code that cleans a CSV file and produces an acceptable output
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant: hi
